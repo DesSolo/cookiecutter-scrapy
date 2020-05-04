0238cbe79b48ab149de5c28ca9d866ba8b3e49e9
DesSolo <dessolo@mail.ru>
2020-05-05 00:20:10 +0400

    Add default params

--- {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/spiders/{{cookiecutter.project_name}}.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/spiders/{{cookiecutter.project_name}}.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+# https://docs.scrapy.org/en/latest/topics/spiders.html
 import scrapy
 from ..items import {{ cookiecutter.object_prefix }}Item
 
@@ -9,4 +10,7 @@ class {{ cookiecutter.object_prefix }}Spider(scrapy.Spider):
     start_urls = ['{{cookiecutter.protocol}}://{{cookiecutter.domain}}']
 
     def parse(self, response):
-        pass
+        yield {'title': response.xpath('//title')}
+        next_page = response.xpath('//a[@class="next" and not(contains(@class, "disabled"))]')
+        if next_page:
+            yield response.follow(next_page.attrib['href'], self.parse)
